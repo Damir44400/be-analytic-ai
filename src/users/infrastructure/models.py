f93edaf640e4db9a08c20356f79eead1c0573b67
dimash <arystambekdimash005@gmail.com>
2025-04-28 13:01:18 +0500

    feat(user): in user model added new fields first name & last name

--- src/users/infrastructure/models.py
+++ src/users/infrastructure/models.py
@@ -8,6 +8,8 @@ class User(Base):
     __tablename__ = 'users'
 
     id: orm.Mapped[int] = orm.mapped_column(sa.Integer, primary_key=True)
+    first_name: orm.Mapped[str] = orm.mapped_column(sa.String, nullable=True)
+    last_name: orm.Mapped[str] = orm.mapped_column(sa.String, nullable=True)
     email: orm.Mapped[str] = orm.mapped_column(sa.String, nullable=False, unique=True)
     password: orm.Mapped[str] = orm.mapped_column(sa.String, nullable=False)
     is_staff: orm.Mapped[bool] = orm.mapped_column(sa.Boolean, nullable=False, default=False)
