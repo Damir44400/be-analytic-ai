647e2867d87dbfebcacbd81bbf2bc5459c7bf3dc
dimash <arystambekdimash005@gmail.com>
2025-04-28 13:03:56 +0500

    feat(user): providers added new use cases retrievers

--- src/users/domain/providers.py
+++ src/users/domain/providers.py
@@ -5,10 +5,15 @@ from src.gateway.config import Config
 from src.gateway.domain.interfaces import IUoW
 from src.users.domain.interfaces import IPasswordBcrypt, IJwtService
 from src.users.domain.use_cases.login.interfaces import ILoginUseCase
+from src.users.domain.use_cases.profile.interfaces import IProfileUseCase, IProfileUpdateUseCase
+from src.users.domain.use_cases.refresh.interfaces import IRefreshUseCase
 from src.users.domain.use_cases.register.interfaces import IRegisterUseCase
 from src.users.infrastructure.dao import UserDAO, IUserDAO
 from src.users.infrastructure.security import PasswordBcrypt, JwtService
 from src.users.use_cases.login_use_case import LoginUseCase
+from src.users.use_cases.profile_update_use_case import ProfileUpdateUseCase
+from src.users.use_cases.profile_use_case import ProfileUseCase
+from src.users.use_cases.refresh_use_case import RefreshUseCase
 from src.users.use_cases.register_use_case import RegisterUseCase
 
 
@@ -18,11 +23,11 @@ class UserProvider(Provider):
     def get_user_dao(self, session: AsyncSession) -> IUserDAO:
         return UserDAO(session)
 
-    @provide(scope=Scope.REQUEST)
+    @provide(scope=Scope.APP)
     def get_password_bcrypt(self) -> IPasswordBcrypt:
         return PasswordBcrypt()
 
-    @provide(scope=Scope.REQUEST)
+    @provide(scope=Scope.APP)
     def get_jwt_service(self, config: Config) -> IJwtService:
         return JwtService(config)
 
@@ -51,3 +56,29 @@ class UserProvider(Provider):
             uow=uow,
             password_bcrypt=password_bcrypt,
         )
+
+    @provide(scope=Scope.REQUEST)
+    def get_profile_use_case(
+            self,
+            user_dao: IUserDAO,
+    ) -> IProfileUseCase:
+        return ProfileUseCase(
+            user_dao=user_dao,
+        )
+
+    @provide(scope=Scope.REQUEST)
+    def get_refresh_use_case(
+            self,
+            jwt_service: IJwtService,
+    ) -> IRefreshUseCase:
+        return RefreshUseCase(
+            jwt_service=jwt_service
+        )
+
+    @provide(scope=Scope.REQUEST)
+    def get_profile_update_use_case(
+            self,
+            uow: IUoW,
+            user_dao: IUserDAO,
+    ) -> IProfileUpdateUseCase:
+        return ProfileUpdateUseCase(uow=uow, user_dao=user_dao)
