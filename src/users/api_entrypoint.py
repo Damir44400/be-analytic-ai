46da9428dc4513caeafc0d62db69487384aed7a2
dimash <arystambekdimash005@gmail.com>
2025-05-08 01:02:28 +0500

    chor(users): removed from user gateway register_user_base_exception_handler

--- src/users/api_entrypoint.py
+++ src/users/api_entrypoint.py
@@ -1,7 +1,5 @@
-from fastapi import APIRouter, Request
-from starlette.responses import JSONResponse
+from fastapi import APIRouter
 
-from .domain.exeptions import UserBaseException
 from .presentation.api.auth import router as auth_router
 from .presentation.api.user import router as user_router
 
@@ -9,6 +7,3 @@ router = APIRouter(prefix="/users")
 
 router.include_router(auth_router, tags=["Auth"])
 router.include_router(user_router, tags=["Profile"])
-
-def register_user_base_exception_handler(request: Request, exc: UserBaseException):
-    return JSONResponse({"error": str(exc)}, status_code=exc.status_code)
