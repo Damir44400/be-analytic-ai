51c50068e4110dbb9cc7fbab7ee3ceaabf2e0d24
dimash <arystambekdimash005@gmail.com>
2025-05-22 21:03:41 -0600

    feat:added logic getting employee

--- src/crm/use_cases/categories/create_category.py
+++ src/crm/use_cases/categories/create_category.py
@@ -2,7 +2,8 @@ from src.core.domain.interfaces import IUoW
 from src.core.exceptions import BadRequestException
 from src.crm.domain.entities.categories import CategoryEntity
 from src.crm.domain.interfaces.daos.categories import ICategoryCreateDAO, ICategoryGetByNameCompanyDAO
-from src.crm.domain.use_cases.categories import CategoryForm
+from src.crm.domain.interfaces.daos.emopoyees import IEmployeeGetByUserCompanyDAO
+from src.crm.infrastructure.models.employees import EmployeeRoleStatusEnum
 
 
 class CategoryGateway(
@@ -16,21 +17,23 @@ class CreateCategoryUseCase:
     def __init__(
             self,
             uow: IUoW,
-            category_gateway: CategoryGateway
+            category_gateway: CategoryGateway,
+            employee_gateway: IEmployeeGetByUserCompanyDAO,
     ):
         self._uow = uow
         self._category_gateway = category_gateway
+        self._employee_gateway = employee_gateway
 
-    async def execute(self, body: CategoryForm) -> CategoryEntity:
+    async def execute(self, body: CategoryEntity, user_id: int) -> CategoryEntity:
+        db_employee = await self._employee_gateway.get_by_user_and_company(user_id, body.company_id)
+        if not db_employee or db_employee.role not in [
+            EmployeeRoleStatusEnum.EMPLOYEE.value,
+            EmployeeRoleStatusEnum.MANAGER.value
+        ]:
+            raise BadRequestException("Employee does not have required role or does not exist")
         db_category = await self._category_gateway.get_by_name(body.name, body.company_id)
         if db_category:
             raise BadRequestException("Category already exists with this given name")
-
         async with self._uow:
-            await self._category_gateway.create(
-                CategoryEntity(
-                    name=body.name,
-                    company_id=body.company_id
-                )
-            )
+            await self._category_gateway.create(body)
         return db_category
